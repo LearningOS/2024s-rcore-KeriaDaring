bac487dd2ed44aa5d3db78026a0d40b5057b7ab7
keria <973289634@qq.com>
2024-07-06 13:29:32 +0800

    init ch1

---
+++ os/target/debug/deps/os-511c4f03c0d09206.d
@@ -0,0 +1,5 @@
+/Users/keria/github-classroom/LearningOS/2024s-rcore-KeriaDaring/os/target/debug/deps/os-511c4f03c0d09206: src/main.rs
+
+/Users/keria/github-classroom/LearningOS/2024s-rcore-KeriaDaring/os/target/debug/deps/os-511c4f03c0d09206.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/Users/keria/github-classroom/LearningOS/2024s-rcore-KeriaDaring/os/target/debug/os: /Users/keria/github-classroom/LearningOS/2024s-rcore-KeriaDaring/os/src/main.rs
